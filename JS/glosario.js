56dc576f13ba44d582c7a486dea78165382aa721
Apolo4444 <shizoudoragon@gmail.com>
2022-10-10 19:18:15 -0300

    Nueva actualización

---
+++ JS/glosario.js
@@ -0,0 +1,59 @@
+let glosario=[
+    {
+        "concepto":"Abierto (sistema)",
+        "definicion":"Sistema que permite la entrada y salida de materia y energia"
+    },
+    {
+        "concepto":"Abiótico",
+        "definicion":"Relación con el ambiente fisicoquímico (no biológico) del medio"
+    },
+    {
+        "concepto":"Aclimatacion",
+        "definicion":"Proceso de "
+    },
+    {
+        "concepto":"Actividad industrial",
+        "definicion":"Aquella que compone la economía de una nación"
+    },
+    {
+        "concepto":"Acuicultura",
+        "definicion":"tecnica de cultivo de plantas y animales acuaticos"
+    },
+    
+    {
+        "concepto":"Abiótico",
+        "definicion":"Relación con el ambiente fisicoquímico (no biológico) del medio"
+    },
+    {
+        "concepto":"Abierto (sistema)",
+        "definicion":"Sistema que permite la entrada y salida de materia y energia"
+    },
+    {
+        "concepto":"Abiótico",
+        "definicion":"Relación con el ambiente fisicoquímico (no biológico) del medio"
+    },
+    {
+        "concepto":"Abierto (sistema)",
+        "definicion":"Sistema que permite la entrada y salida de materia y energia"
+    },
+    {
+        "concepto":"Abiótico",
+        "definicion":"Relación con el ambiente fisicoquímico (no biológico) del medio"
+    },
+    {
+        "concepto":"Abierto (sistema)",
+        "definicion":"Sistema que permite la entrada y salida de materia y energia"
+    },
+    {
+        "concepto":"Abiótico",
+        "definicion":"Relación con el ambiente fisicoquímico (no biológico) del medio"
+    },
+    {
+        "concepto":"Abierto (sistema)",
+        "definicion":"Sistema que permite la entrada y salida de materia y energia"
+    },
+    {
+        "concepto":"Abiótico",
+        "definicion":"Relación con el ambiente fisicoquímico (no biológico) del medio"
+    },
+];
