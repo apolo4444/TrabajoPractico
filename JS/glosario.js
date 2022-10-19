4b3cb5c417e8481507bada04da11f83e9258a14c
Apolo4444 <shizoudoragon@gmail.com>
2022-10-19 13:04:46 -0300

    Nueva actualizacion

--- JS/glosario.js
+++ JS/glosario.js
@@ -1,68 +1,73 @@
-let glosario=[
+
+const glosario=[
     {
-        "concepto":"Abierto (sistema)",
-        "definicion":"Sistema que permite la entrada y salida de materia y energia"
+        concepto:"Abierto (sistema)",
+        definicion:"Sistema que permite la entrada y salida de materia y energia"
     },
     {
-        "concepto":"Abiótico",
-        "definicion":"Relación con el ambiente fisicoquímico (no biológico) del medio"
+        concepto:"Abiótico",
+        definicion:"Relación con el ambiente fisicoquímico (no biológico) del medio"
     },
     {
-        "concepto":"Aclimatacion",
-        "definicion":"Proceso de "
+        concepto:"Aclimatacion",
+        definicion:"Proceso de ajuste fisiológico de un organismo a nuevas condiciones ambientales"
     },
     {
-        "concepto":"Actividad industrial",
-        "definicion":"Aquella que compone la economía de una nación"
+        concepto:"Actividad industrial",
+        definicion:"Aquella que compone la economía de una nación"
     },
     {
-        "concepto":"Acuicultura",
-        "definicion":"tecnica de cultivo de plantas y animales acuaticos"
+        concepto:"Acuicultura",
+        definicion:"tecnica de cultivo de plantas y animales acuaticos"
     },
     
     {
-        "concepto":"Abiótico",
-        "definicion":"Relación con el ambiente fisicoquímico (no biológico) del medio"
-    },
-    {
-        "concepto":"Abierto (sistema)",
-        "definicion":"Sistema que permite la entrada y salida de materia y energia"
-    },
-    {
-        "concepto":"Abiótico",
-        "definicion":"Relación con el ambiente fisicoquímico (no biológico) del medio"
-    },
-    {
-        "concepto":"Abierto (sistema)",
-        "definicion":"Sistema que permite la entrada y salida de materia y energia"
-    },
-    {
-        "concepto":"Abiótico",
-        "definicion":"Relación con el ambiente fisicoquímico (no biológico) del medio"
-    },
-    {
-        "concepto":"Abierto (sistema)",
-        "definicion":"Sistema que permite la entrada y salida de materia y energia"
-    },
-    {
-        "concepto":"Abiótico",
-        "definicion":"Relación con el ambiente fisicoquímico (no biológico) del medio"
-    },
-    {
-        "concepto":"Abierto (sistema)",
-        "definicion":"Sistema que permite la entrada y salida de materia y energia"
-    },
-    {
-        "concepto":"Abiótico",
-        "definicion":"Relación con el ambiente fisicoquímico (no biológico) del medio"
-    },
+        concepto:"Adaptacion",
+        definicion:"Cambio evolutivo que ocurre en una población como producto de la acción de la selección natural"
+    }
+    // {
+    //     "concepto":"Abierto (sistema)",
+    //     "definicion":"Sistema que permite la entrada y salida de materia y energia"
+    // },
+    // {
+    //     "concepto":"Abiótico",
+    //     "definicion":"Relación con el ambiente fisicoquímico (no biológico) del medio"
+    // },
+    // {
+    //     "concepto":"Abierto (sistema)",
+    //     "definicion":"Sistema que permite la entrada y salida de materia y energia"
+    // },
+    // {
+    //     "concepto":"Abiótico",
+    //     "definicion":"Relación con el ambiente fisicoquímico (no biológico) del medio"
+    // },
+    // {
+    //     "concepto":"Abierto (sistema)",
+    //     "definicion":"Sistema que permite la entrada y salida de materia y energia"
+    // },
+    // {
+    //     "concepto":"Abiótico",
+    //     "definicion":"Relación con el ambiente fisicoquímico (no biológico) del medio"
+    // },
+    // {
+    //     "concepto":"Abierto (sistema)",
+    //     "definicion":"Sistema que permite la entrada y salida de materia y energia"
+    // },
+    // {
+    //     "concepto":"Abiótico",
+    //     "definicion":"Relación con el ambiente fisicoquímico (no biológico) del medio"
+    // },
 ];
 
 
-let table="";
+var table=``;
 
-for(concepto in glosario){
-    table+=`<tr><td>${concepto.concepto}</td><td>${concepto.definicion}</td></tr>`;
-}
+    for( let concepto of glosario){
+        table+=`<tr><td>${concepto.concepto}</td><td>${concepto.definicion}</td></tr> <br>`;
+    }
+    document.getElementById("glosario").innerHTML=table;
 
-document.getElementsByClassName("glosario").innerHTML=table;
+
+
+
+
