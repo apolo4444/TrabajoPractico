ce40cfab9b03b0e1729949724f676fe3a63addf4
Apolo4444 <shizoudoragon@gmail.com>
2022-10-19 13:06:17 -0300

    Nueva actualizacion

---
+++ JS/main.js
@@ -0,0 +1,40 @@
+const { createApp } = Vue  //creo un objeto VUE llamdo createApp
+
+  createApp({
+    data() {
+      return {
+        number:0,
+        url:"./.vscode/glosario.json",
+        conceptos:[],
+        error:false,
+        nroerror:0,
+        cargando:true
+        
+      } 
+    },
+    methods: {
+      fetchData(url) {
+
+          fetch(url)
+              .then(response => response.json())
+              .then(data => { 
+                this.conceptos=data
+                this.cargando=false
+                 }
+              )
+              .catch(error=>{
+                console.log("error"+error)
+                this.error=true
+                this.nroerror=error
+              }
+              )
+      }
+    },
+    created(){
+
+      this.fetchData(this.url) 
+    } 
+
+  }).mount('#app')
+
+ 
